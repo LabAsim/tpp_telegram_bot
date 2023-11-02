61a749a4dc246f6d61650264b7b735938289bbe4
LabAsim <lampgit@pm.me>
2023-11-02 23:22:33 +0200

    cleaning

--- source/helper/helper.py
+++ source/helper/helper.py
@@ -7,14 +7,8 @@ import copy
 import logging
 import os.path
 import pathlib
-import re
-import traceback
-from datetime import datetime
-from functools import wraps, partial
-
 import colorama
-from colorama import Fore, Style
-from source.helper.misc import dir_path
+from functools import wraps, partial
 from typing import Union, Callable
 
 
@@ -94,70 +88,6 @@ def file_exists(dir_path: Union[str, os.PathLike], name: str) -> bool:
         return False
 
 
-def strip_ansi_characters(text="") -> str:
-    """https://stackoverflow.com/questions/48782529/exclude-ansi-escape-sequences-from-output-log-file"""
-    try:
-        ansi_re = re.compile(r"\x1b\[[0-9;]*m")
-        return re.sub(ansi_re, "", text)
-    except re.error as err:
-        print(err)
-
-
-def get_current_time() -> str:
-    """
-    :return: The current datetime as a string
-    """
-    time_now = datetime.now()
-    dt = (
-        str(time_now.strftime("%d-%m-%Y %H:%M:%S"))
-        + f".{Fore.LIGHTBLACK_EX}{str(round(time_now.microsecond))[:4]}"
-        f"{Style.RESET_ALL}"
-    )
-    dt = f"[{dt}]\t"
-    return dt
-
-
-def cprint(text, to_print_time=True):
-    """Prints and timestamps the text"""
-    text = str(text)  # Be sure that text is a string.
-    if to_print_time:
-        print(f"{strip_ansi_characters(get_current_time())}{text}")
-    elif not to_print_time:
-        print(f"{text}")
-
-
-def trace_error(to_print_error=True):
-    """
-    The function traces the exceptions by printing and logging the error.
-
-    :param to_print_error: Boolean - If true, prints and logs. Else, its logs the errors in a separate txt file
-    :return: None
-
-    See also:
-    # https://docs.python.org/3/library/traceback.html#traceback-examples
-    """
-    # exc_type, exc_value, exc_traceback = sys.exc_info()  # All the info from the exception
-    formatted_lines = traceback.format_exc().splitlines()
-    error_filepath = os.path.join(dir_path, "errors.txt")
-    error_data = ""
-    # Alternative, just print and write traceback.format_exc()
-    for line in formatted_lines:
-        if to_print_error:
-            cprint(line)
-        if file_exists(dir_path, "errors.txt"):
-            with open(error_filepath, "r+", encoding="utf-8") as file:
-                file_contents = file.read()
-                error_data = f"{file_contents}{strip_ansi_characters(get_current_time())} {line}\n"
-            with open(error_filepath, "w+", encoding="utf-8") as file:
-                file.write(strip_ansi_characters(error_data))
-        else:
-            with open(error_filepath, "w+", encoding="utf-8") as file:
-                file.write(
-                    f"{strip_ansi_characters(get_current_time())}{line}\n"
-                )  # Add \n in the end to format nicely
-    print(f"Errors saved in {error_filepath}")
-
-
 def wrap_as_async(func) -> Callable:
     @wraps(func)
     async def run(*args, loop=None, executor=None, **kwargs):
@@ -202,12 +132,3 @@ def color_logging(level: int) -> logging.StreamHandler:
     # tell the handler to use this format
     console.setFormatter(formatter)
     return console
-
-
-if __name__ == "__main__":
-    # Example for trace_error()
-    try:
-        a = 2
-        a + "b"
-    except Exception:
-        trace_error()
